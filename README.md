630ceca972ba6174acb7bde8f67cba7b63b2888b
SnailMath <snailmathprog@gmail.com>
2021-08-11 17:31:44 +0200

    Add ability to change values during runtime

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-A program for the fx-cp400 that draws the Barnsley fern. 
+# CPfractal
 
-This requires hollyhock-2 (or hollyhock) for the classpad II.
+A program for the fx-cp400 that draws the Barnsley fern (and other fractals). 
 
-This will work on the fx-cp400, fx-cp400+E, fx-cg500 and on Linux or WSL.
+This requires [hollyhock-2](htts://www.github.com/SnailMath/hollyhock-2) (or hollyhock) for the classpad II and it will work on the fx-cp400, fx-cp400+E, fx-cg500 and on Linux or WSL. Only tested it under WSL, but it works. (Until now, we haven't tried any program on the cp400+E or cg500 yet, but they are assumed to have the same hardware)
 
-Only tested it under WSL, but it works.
+## Usage
 
-Start the program, it will draw the fern consisting of 4000 points. The screen is updatet after every 100 points and Clear or EXE closes the program.
+Start the program, it will draw the fern consisting of 40000 points. The screen is updatet after every 100 (or 1000) points and Clear or EXE closes the program.
+They key 5 turns the overlay on or off, the dot ('.') adds 40000 more points.
 
-If you want, you can change the values to another fractal and recompile the code.
+## Modifying the fractal
+
+If you want, you can change the values. The values for each equation are in a matrix:
+
+|  a  |  b  |  e  |
+| --- | --- | --- |
+|  c  |  d  |  f  |
+
+There are four equations, each is displayed with a red rectangle. You can press 7 to change the rectangle you want to edit (displayed in a stronger red) and you can perss 9 to
+change the value pair to edit (the 2 parts of the rotation or the translation). You can edit the selected value with the arrow keys or with the numpad 2,4,6,8 ("Nokia Style").
+
+You can use 1 and 3 to change the speed of the change, press 1 multiple times to reduce the step width, press 3 to make it bigger, default is 0.001
+
--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 
 #ifndef PC
 	APP_NAME("CPfactals")
-	APP_DESCRIPTION("A short description of my app")
-	APP_AUTHOR("ThatLolaSnail")
-	APP_VERSION("0.0.0")
+	APP_DESCRIPTION("Fractals. Use num/arrow keys, 7+9 change selecction, 1+3 change step width, 5 toggle, '.' draw more.")
+	APP_AUTHOR("SnailMath")
+	APP_VERSION("1.0.0")
 #endif
+
+#define MAXS 4
+
+//#define Y(y) (height-((height-width)/2)-y)
+#define Y(y) (height-y)
 
 //RNG inspired by https://github.com/Qqwy/SimpleRNG/blob/master/c/simple_rng.c
 //class RNG {
@@ -21,7 +26,7 @@
 
 class Fractal {
 public:
-	float x = 160; //If the first point is already in the fractal, there are no ugly points outside the fractal
+	float x = 160;
 	float y =   0;
 	float a[4] = {	 0.849,	 0.197,	-0.150,	 0.000	};
 	float b[4] = {	 0.037,	-0.226,	 0.283,	 0.000	};
@@ -32,20 +37,28 @@ public:
 	//		  70%	  13%	  15%	   2%	//percentage for each thing
 	//		  70%	  83%	  98%	 100%	//percentage added
 	uint8_t p[4]={	 179,	 212,	 251,	 255	};
-	//uint8_t p[4]={	 63,	 127,	 191,	 255	};
+	//uint8_t p[S]={	 63,	 127,	 191,	 255	};
 	
+	//void init(){
+		//x = 160; //If the first point is already in the fractal, there are no ugly points outside the fractal
+		//y = 0;
+		//amount=40000; //the amount of pixels required
+	//}
+
+	//use a random equation
 	void step(){
 		int s=0;
 		int r = nextRand() & 0xff;
 		
 		while ( r > p[s] ) s++;
-		//println("r=%d s=%d",(int)r,(int)s);
-
+		calc(s,x,y,&x,&y);
+		setPixel((int)x, Y((int)y), color(0,0,0) );
+	}
+	//use a specific equation
+	void calc(int s,float x,float y, float* outx, float* outy){
 		float u = (a[s]*x) + (b[s]*y) + (e[s]*width);
 		float v = (c[s]*x) + (d[s]*y) + (f[s]*width);
-		x=u; y=v;
-		setPixel((int)x, height-((height-width)/2)-(int)y, color(0,0,0) );
-		//println("x=%d y=%d",(int)x,(int)y);
+		*outx = u; *outy = v;
 
 	}
 
@@ -54,36 +67,137 @@ public:
 //The acutal main
 void main2(){
 	
-	//Clear the screen
-	fillScreen(color(255,255,255));
-	LCD_Refresh();
-
 	//Create the fractal object
-	Fractal fractal;
+      Fractal fractal;
 
 	//Use the fractal object to draw a fractal
+	
+	//iniyialize variables
+	bool keyPressed=false; //if a key is currently being held down
+	uint8_t es = 0 ; //edit s (which square is selected?)
+	uint8_t ev = 2 ; //edit value (which value is selected)
+	bool redraw=true;//do we need to start from scratch?
+	long amount = 40000; //the amount that still needs to be drawn
+	float keystep = 0.001; //the step with each keypress
+	bool visible = true; //if boxes are visible
 
 	//repeat until a key is pressed
-	int drawing = 400; //The amount of points divided by 100
 	while(true){
+	
+		//Clear the screen
+		if(redraw){
+			fillScreen(color(255,255,255));
+			randomInt = 1; //Reset the RNG
+			amount = 40000;
+			redraw=false;
+			//fractal.init();
+		}
 		
-		if (drawing){
-			//calculate 100 points
-			for(long i=0; i<100; i++)
+		//calculate 1000 points
+		if(amount){ //if the fractal is not fully drawn yet
+			for(long i=0; i<1000; i++)
 				fractal.step();
-			//Update the screen
-			LCD_Refresh();
-			drawing--;
+			amount-=1000;
 		}
+			
+		//draw boxes to show the equations
+		if(visible)
+			for (int s=0; s<MAXS; s++){
+				float x0,y0,x1,y1,x2,y2,x3,y3;
+				fractal.calc(s,    0,     0,&x0,&y0);
+				fractal.calc(s,width,     0,&x1,&y1);
+				fractal.calc(s,width,height,&x2,&y2);
+				fractal.calc(s,    0,height,&x3,&y3);
+				#define sel color(255,0,0)
+				#define uns color(255,192,192)
+				line(x0,Y(y0),x1,Y(y1),(es!=s)?uns:(ev!=1?sel:uns));
+				line(x1,Y(y1),x2,Y(y2),(es!=s)?uns:(ev==2?sel:uns));
+				line(x2,Y(y2),x3,Y(y3),(es!=s)?uns:(ev==2?sel:uns));
+				line(x3,Y(y3),x0,Y(y0),(es!=s)?uns:(ev!=0?sel:uns));
+			}
+		//Update the screen
+		LCD_Refresh();
 
-		uint32_t key1, key2;
-		getKey(&key1, &key2);
-		if(testKey(key1, key2, KEY_CLEAR)){
-			break;
-		}
-		if(testKey(key1, key2, KEY_EXE)){
-			break;
+      		uint32_t key1, key2;
+      		getKey(&key1, &key2);
+      		if(key1||key2){
+			if(!keyPressed){
+				//If the key was pressed, not held
+				if(testKey(key1, key2, KEY_7)){
+					es++; if(es==MAXS) es=0;
+				}
+				if(testKey(key1, key2, KEY_9)){
+					ev++; if(ev==3) ev=0;
+				}
+				if(testKey(key1, key2, KEY_1)){
+					keystep *=.5;
+				}
+				if(testKey(key1, key2, KEY_3)){
+					keystep *=2;
+				}
+				if(testKey(key1, key2, KEY_DOT)){
+					amount = 40000;
+				}
+				if(testKey(key1, key2, KEY_5)){
+					if (visible){
+						visible = false;
+						redraw = true;
+					} else {
+						visible = true;
+					}
+				}
+			}
+			//Pressed or held down
+			//up (y++) or key 8
+			if(testKey(key1, key2, KEY_8)
+			|| testKey(key1, key2, KEY_UP)){
+				if(ev==0) fractal.c[es]=fractal.c[es]+keystep;
+				if(ev==1) fractal.d[es]=fractal.d[es]+keystep;
+				if(ev==2) fractal.f[es]=fractal.f[es]+keystep;
+				redraw=true;
+			}
+			//down (y--) or key 2
+			if(testKey(key1, key2, KEY_2)
+			|| testKey(key1, key2, KEY_DOWN)){
+				if(ev==0) fractal.c[es]=fractal.c[es]-keystep;
+				if(ev==1) fractal.d[es]=fractal.d[es]-keystep;
+				if(ev==2) fractal.f[es]=fractal.f[es]-keystep;
+				redraw=true;
+			}
+			//left (x--) or key 4
+			if(testKey(key1, key2, KEY_4)
+			|| testKey(key1, key2, KEY_LEFT)){
+				if(ev==0) fractal.a[es]=fractal.a[es]-keystep;
+				if(ev==1) fractal.b[es]=fractal.b[es]-keystep;
+				if(ev==2) fractal.e[es]=fractal.e[es]-keystep;
+				redraw=true;
+			}
+			//left (x++) or key 6
+			if(testKey(key1, key2, KEY_6)
+			|| testKey(key1, key2, KEY_RIGHT)){
+				if(ev==0) fractal.a[es]=fractal.a[es]+keystep;
+				if(ev==1) fractal.b[es]=fractal.b[es]+keystep;
+				if(ev==2) fractal.e[es]=fractal.e[es]+keystep;
+				redraw=true;
+			}
+
+			//if(testKey(key1, key2, KEY_3)){
+			//	fractal.f[0]=fractal.f[0]-.01;
+			//	redraw=true;
+			//}
+			//if(testKey(key1, key2, KEY_4)){
+			//	fractal.f[0]=fractal.f[0]+.01;
+			//	redraw=true;
+			//}
+			if(testKey(key1, key2, KEY_CLEAR)){
+				break;
+			}
+			if(testKey(key1, key2, KEY_EXE)){
+				break;
+			}
+			keyPressed=true;
+		}else{
+			keyPressed=false;
 		}
 	}
-
 }
